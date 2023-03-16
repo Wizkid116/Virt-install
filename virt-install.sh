271fc0c8a55323d36280d8a45f2dac1db1dbbe2c
Wizkid116 <114298273+Wizkid116@users.noreply.github.com>
2023-03-16 07:28:42 -0500

    Update virt-install.sh

--- virt-install.sh
+++ virt-install.sh
@@ -1,4 +1,4 @@
-#Phase 1 script for installing QEMU onto any arch-based distro with SystemD support.
+# Shell script for installing QEMU onto any arch-based distro with SystemD support.
 echo "downloading dependencies"
 sudo pacman -Syy
 sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat libguestfs
@@ -11,5 +11,5 @@ sudo awk '/^#.*unix_sock_ro_perms/{sub(/^#/,"",$0)}1' /etc/libvirt/libvirtd.conf
 sudo awk '/^#.*unix_sock_rw_perms/{sub(/^#/,"",$0)}1' /etc/libvirt/libvirtd.conf > temp && sudo mv temp /etc/libvirt/libvirtd.conf
 username=$USER
 sudo usermod -aG libvirt $username
-echo "Phase 1 complete, restart your system for changes to take effect."
+echo "Installation complete, restart your system for changes to take effect."
 
